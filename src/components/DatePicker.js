3a2a9a10bff5f259d5f7a63711a6504d56bbd4b0
Aleksandr Tagunov <aleksandr@tagunovdesign.com>
2020-10-10 21:23:48 -0700

    style date-picker component

--- src/components/DatePicker.js
+++ src/components/DatePicker.js
@@ -1,17 +1,19 @@
 import React, { useContext } from "react";
 import { AuthContext } from "../Auth";
 import { parseISO, compareAsc, subDays, format, addDays } from "date-fns";
+import "../css/date-picker.scss";
 export default function DatePicker() {
   const { dateContext } = useContext(AuthContext);
   const [date, setDate] = dateContext;
   return (
-    <div>
+    <div className="date-picker">
       <button
+        className="date-picker-btn"
         onClick={() => {
           setDate(format(subDays(parseISO(date), 1), "yyyy-MM-dd"));
         }}
       >
-        -
+        <strong> &lt;</strong>
       </button>
 
       <input
@@ -22,12 +24,13 @@ export default function DatePicker() {
       />
 
       <button
+        className="date-picker-btn"
         onClick={() => {
           if (compareAsc(addDays(parseISO(date), 1), new Date()) < 0)
             setDate(format(addDays(parseISO(date), 1), "yyyy-MM-dd"));
         }}
       >
-        +
+        <strong>&gt;</strong>
       </button>
     </div>
   );
