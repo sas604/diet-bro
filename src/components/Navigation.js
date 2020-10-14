39310356d0f11cdd08b7d646fdc15a008695b1d5
Aleksandr Tagunov <aleksandr@tagunovdesign.com>
2020-10-13 20:09:51 -0700

    style global navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,6 +6,7 @@ import { ReactComponent as HomeSvg } from "../img/home-solid.svg";
 import { ReactComponent as WeightSvg } from "../img/weight-solid.svg";
 import { ReactComponent as GraphSvg } from "../img/chart-line-solid.svg";
 import { ReactComponent as CogSvg } from "../img/cog-solid.svg";
+import "../css/nav.scss";
 
 export default function Navigation() {
   const { currentUser } = useContext(AuthContext);
@@ -13,28 +14,21 @@ export default function Navigation() {
 
   return (
     <nav className="global-nav">
-      <ul style={{ display: "flex", listStyleType: "none" }}>
-        <li>
-          <NavLink to={ROUTES.HOME}>
-            <HomeSvg style={{ width: 50 }} />
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to={ROUTES.WEIGHT}>
-            <WeightSvg style={{ width: 50 }} />
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to={ROUTES.STATS}>
-            <GraphSvg style={{ width: 50 }} />
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to={ROUTES.ACCOUNT}>
-            <CogSvg style={{ width: 50 }} />
-          </NavLink>
-        </li>
-      </ul>
+      <NavLink to={ROUTES.HOME}>
+        <HomeSvg />
+      </NavLink>
+
+      <NavLink to={ROUTES.WEIGHT}>
+        <WeightSvg />
+      </NavLink>
+
+      <NavLink to={ROUTES.STATS}>
+        <GraphSvg />
+      </NavLink>
+
+      <NavLink to={ROUTES.ACCOUNT}>
+        <CogSvg />
+      </NavLink>
     </nav>
   );
 }
