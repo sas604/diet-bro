dbfcb5f172ec02684d35eff6e43dec50cb01a1a9
Aleksandr Tagunov <aleksandr@tagunovdesign.com>
2020-10-16 15:03:04 -0700

    Replace li with button for accessability

--- src/components/SearchResult.js
+++ src/components/SearchResult.js
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function SearchResult({ id, handleClick, name, calories }) {
   return (
-    <li className="search-result-list-item" id={id} onClick={handleClick}>
+    <button className="search-result-list-item" id={id} onClick={handleClick}>
       <p>{name}</p>
       <p>{calories}kcal/100g</p>
-    </li>
+    </button>
   );
 }
