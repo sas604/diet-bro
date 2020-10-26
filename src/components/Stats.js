b27b42b09afdc4a4df9d24924d2a5c4261acf954
Aleksandr Tagunov <aleksandr@tagunovdesign.com>
2020-10-26 10:34:59 -0700

    fix header

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Stats() {
   return (
     <div className="wrapper bg-pattern">
-      <h1>Your Statistic </h1>
+      <h1>Statistic </h1>
 
       <p>Sorry, this section is still under development </p>
     </div>
